3ea30bcb8c2ec31899eed81a151d055dd0137ff4
Ben Green <ben@bcgdesign.com>
2022-02-22 15:27:49 +0000

    Using new Apache base image

---
+++ generate-dockerfiles.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -euo pipefail
+
+docker pull bfren/alpine
+
+BASE_REVISION="2.0.4"
+echo "Base: ${BASE_REVISION}"
+
+PHP_VERSIONS="5.6"
+for V in ${PHP_VERSIONS} ; do
+
+    echo "PHP ${V}"
+    PHP_MAJOR="$(echo ${V} | cut -c 1)"
+    APACHE_BASE=`cat ./${V}/APACHE_BASE`
+
+    DOCKERFILE=$(docker run \
+        -v ${PWD}:/ws \
+        -e BF_DEBUG=0 \
+        bfren/alpine esh \
+        "/ws/Dockerfile.esh" \
+        BASE_REVISION=${BASE_REVISION} \
+        APACHE_BASE=${APACHE_BASE} \
+        PHP_MAJOR=${PHP_MAJOR} \
+        PHP_MINOR=${V}
+    )
+
+    echo "${DOCKERFILE}" > ./${V}/Dockerfile
+
+done
+
+docker system prune -f
+echo "Done."
